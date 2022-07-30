74ca11739b0e41608ddf047339308e92c44560ca
Lauri Salmi <lasalmi@student.hive.fi>
2022-07-30 17:02:51 +0300

    Exercise 1.11

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,85 +1,39 @@
 import { useState } from 'react'
 
-	
-// const Button = ({handleClick, text}) => (
-// 	<button onClick={handleClick}> 
-// 	{text} 
-// 	</button>
-// )
-
-// const History = (props) => {
-// 	if (props.allClicks.length === 0) {
-// 		return (
-// 			<div>
-// 			The App is used by pressing the buttons
-// 			</div>
-// 		)
-// 	}
-// 	return (
-// 		<div>
-// 			Button press history: {props.allClicks.join(' ')}
-// 		</div>
-// 	)
-// }
-
-// const App = () => {
-// 	const [left, setLeft] = useState(0)
-// 	const [right, setRight] = useState(0)
-// 	const [allClicks, setAll] = useState([])
-
-// 	const handleLeftClick = () => {
-// 		setAll(allClicks.concat('L'))
-// 		setLeft(left + 1)
-// 	}
-// 	const handleRightClick = () => {
-// 		setAll(allClicks.concat('R'))
-// 		setRight(right + 1)
-// 	}
-// 	return (
-// 		<div>
-// 			<div>
-// 				{left}
-// 				<Button handleClick={handleLeftClick} text='Left'/>
-// 				<Button handleClick={handleRightClick} text='Right' />
-// 				{right}
-// 				<History allClicks={allClicks}/>
-// 			</div>
-// 		</div>
-// 	)
-// 	}
-
 const Header = ({text}) => (<h1>{text}</h1>)
 
+const StatisticsLine = (props) => {
+	if (props.text === 'positive') {
+		return (
+			<tr>
+			<td>{props.text}</td><td>{props.value} %</td>
+			</tr>
+		)
+	}
+	return (
+		<tr>
+		<td>{props.text}</td><td>{props.value}</td>
+		</tr>
+	)
+}
 const Statistics = (props) => {
 	<Header text='Statistics'/>
 	if (props.total === 0) {
 		return (<p>No feedback given</p>)
 	}
 	return (
-		<p>Good: {props.good} Neutral: {props.neutral} Bad: {props.bad} Total: {props.total}
-		<Average good={props.good} bad={props.bad} total={props.total}/>
-		<Positive good={props.good} total={props.total}/></p>
-	)
-}
-const Average = ({good, bad, neutral, total}) => {
-	if (total === 0) {
-		return (<p>Average: 0</p>)
-	}
-	return (
-		<p>Average: {(good * 1 + bad * -1) / total}</p>
+		<table>
+			<tbody>
+			<StatisticsLine text="good" value={props.good}/>
+			<StatisticsLine text="neutral" value={props.neutral}/>
+			<StatisticsLine text="bad" value={props.bad}/>
+			<StatisticsLine text="average" value={(props.good - props.bad) / props.total}/>
+			<StatisticsLine text="positive" value={props.good / props.total * 100}/>
+			</tbody>
+		</table>
 	)
 }
 
-const Positive = ({good, total}) => {
-	return (
-		<p>Positive: {good / total * 100}%</p>
-	)
-}
-const Display = (props) => {
-	return (
-		<p>Good: {props.good} Neutral: {props.neutral} Bad: {props.bad} Total: {props.total}</p>
-	)
-}
 const Button = ({handleClick, text}) => (
 	<button onClick={handleClick}> 
 	{text} 
